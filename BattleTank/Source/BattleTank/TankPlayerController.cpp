4ae90dae35efafd88523d0f657d1a17dbc9dac05
Laura Grondahl <emailgrondahl@gmail.com>
2018-12-27 16:59:31 -0800

    check for possessed tank on begin play

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,9 +2,25 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController begin play"));
+
+	/// Log out possessed tank
+	ATank* PossessedTank = GetControlledTank();
+	if (!PossessedTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No Possessed Tank")); 
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed Tank %s"), *PossessedTank->GetFName().ToString());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
-
+	return Cast<ATank>(GetPawn());
 }
 
