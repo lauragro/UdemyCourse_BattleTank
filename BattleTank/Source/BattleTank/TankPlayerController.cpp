e631e67e9150cbf15bd2ec6f926a0318235e4c17
Laura Grondahl <emailgrondahl@gmail.com>
2018-12-31 15:55:48 -0800

    get look direction of camera through crosshair

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -38,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	// Get world location of line through crosshair
 	if (GetSightRayHitLocation(OutHitLocation))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *OutHitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *OutHitLocation.ToString());
 	}
 
 	// If we hit the landscape
@@ -47,8 +47,32 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
 {
+	/// Find the crosshair screen position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	//UE_LOG(LogTemp, Warning, TEXT("Crosshair screen location: %s"), *ScreenLocation.ToString());
+
+	/// "de-project" the screen position to a world position
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *LookDirection.ToString());
+	}
+
+	// ray-trace along the look direction andd see what we hit
+
 	OutHitLocation = FVector(1.0);
 
 	return true;
 }
 
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& OutLookDirection) const
+{
+	FVector CameraWorldLocation; // To be discarded
+	return DeprojectScreenPositionToWorld(
+		ScreenLocation.X, 
+		ScreenLocation.Y, 
+		CameraWorldLocation, 
+		OutLookDirection);
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,11 +16,20 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CrosshairYLocation = 0.33333;
+
+
 	ATank* GetControlledTank() const;
 	
 	void BeginPlay() override;
 
 private:
+
+
 	void Tick(float DeltaTime) override;
 
 	// Start the tank moving the barrel so that a bullet would hit
@@ -29,4 +38,5 @@ private:
 
 	// Return an OUT parameter, TRUE if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& OutLookDirection) const;
 };
